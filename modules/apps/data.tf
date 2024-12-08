9ac657e7d20765ee8cdc725941eed507890d7abd
shuja <shuja.devops@gmail.com>
2024-12-09 04:58:50 +0530

    owners id fixed

--- modules/apps/data.tf
+++ modules/apps/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "RHEL-9-DevOps-Practice"
-  owners           = ["471112569439"]
+  owners           = ["973714476881"]
 }
 
 data "aws_security_group" "selected" {
