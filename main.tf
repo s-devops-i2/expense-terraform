62431d13e9b77b4f1322f9da359243b6eb761052
shuja <shuja.devops@gmail.com>
2024-12-09 09:08:10 +0530

    backend and mysql component setup

--- main.tf
+++ main.tf
@@ -8,3 +8,25 @@ module "frontend" {
   zone_id       = var.zone_id
 
 }
+
+module "backend" {
+  source = "./modules/apps"
+  instance_type = var.instance_type
+  component     ="backend"
+  ssh_user      = var.ssh_user
+  ssh_password  = var.ssh_password
+  env           = var.env
+  zone_id       = var.zone_id
+
+}
+
+module "mysql" {
+  source = "./modules/apps"
+  instance_type = var.instance_type
+  component     ="mysql"
+  ssh_user      = var.ssh_user
+  ssh_password  = var.ssh_password
+  env           = var.env
+  zone_id       = var.zone_id
+
+}
