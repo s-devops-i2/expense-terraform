f2e96f36573cdc4ca5a80e1db5a781165c218287
shuja <shuja.devops@gmail.com>
2024-12-13 17:39:17 +0530

    tag added

--- modules/apps/main.tf
+++ modules/apps/main.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 
   tags = {
-    Name = var.component
+    Name    = var.component
+    monitor = "yes"
   }
 }
 resource "null_resource" "ansible" {
