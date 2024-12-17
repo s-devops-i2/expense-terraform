162286fe2458e16564b35236f0c3ae95529591b4
shuja <shuja.devops@gmail.com>
2024-12-17 19:17:40 +0530

    installing hvac for ansible to talk to vault

--- modules/apps/main.tf
+++ modules/apps/main.tf
@@ -20,9 +20,9 @@ resource "null_resource" "ansible" {
       }
 
       inline = [
-        "sudo pip3.11 install ansible",
+        "sudo pip3.11 install ansible hvac",
         "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
-        "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git expense-play.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json"
+        "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git expense-play.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json -e @~/app.json"
 
       ]
   }
