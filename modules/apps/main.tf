f8d82023303d232d93034bbd742ed5d7e9bcef1d
shuja <shuja.devops@gmail.com>
2024-12-17 18:54:52 +0530

    update file path

--- modules/apps/main.tf
+++ modules/apps/main.tf
@@ -21,7 +21,8 @@ resource "null_resource" "ansible" {
 
       inline = [
         "sudo pip3.11 install ansible",
-        "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git expense-play.yml -e env=${var.env} -e role_name=${var.component}"
+        "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
+        "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git expense-play.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json"
 
       ]
   }
