440dea65d72f22d83dacd3ef2a8859db12944ada
shuja <shuja.devops@gmail.com>
2024-12-18 11:42:42 +0530

    commented frontend module

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-module "frontend" {
-  depends_on    = [module.backend]
-  source        = "./modules/apps"
-  instance_type = var.instance_type
-  component     ="frontend"
-  env           = var.env
-  zone_id       = var.zone_id
-  vault_token   = var.vault_token
-
-}
+# module "frontend" {
+#   depends_on    = [module.backend]
+#   source        = "./modules/apps"
+#   instance_type = var.instance_type
+#   component     ="frontend"
+#   env           = var.env
+#   zone_id       = var.zone_id
+#   vault_token   = var.vault_token
+#
+# }
 
 module "backend" {
   depends_on    = [module.mysql]
