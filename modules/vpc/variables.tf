934b8086eb826541c198c46df0c45fe0c12f59ca
shuja <shuja.devops@gmail.com>
2024-12-23 11:22:53 +0530

    fixed var deleting

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,6 +1,6 @@
 variable "dev_vpc_cidr_block" {}
 variable "env" {}
-variable "dev_subnet_cidr_block" {}
+
 variable "default_vpc_id" {}
 variable "dev_route_table_id" {}
 variable "default_rout_table_id" {}
