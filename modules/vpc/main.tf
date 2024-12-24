5a02001fd9cbcea236d58e7682efff2661ced284
shuja <shuja.devops@gmail.com>
2024-12-24 11:55:17 +0530

    public route table associated with public subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -67,6 +67,11 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table_association" "public" {
+  count      = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
+}
 
 
 resource "aws_vpc_peering_connection" "foo" {
