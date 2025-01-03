93f79c7883d9be90a9d4eba975b7136cbff2c80e
shuja <shuja.devops@gmail.com>
2025-01-03 11:25:12 +0530

    updating common/single secrets file

--- modules/apps/main.tf
+++ modules/apps/main.tf
@@ -50,14 +50,14 @@ resource "null_resource" "ansible" {
     provisioner "remote-exec" {
       inline = [
         "sudo pip3.11 install ansible hvac",
-        "rm -f ~/secrets.json ~/app.json",
+        "rm -f ~/*.json",
         "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
-        "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git expense-play.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json -e @~/app.json"
+        "ansible-pull -i localhost, -U https://github.com/s-devops-i2/expense-ansible2.git expense-play.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json"
       ]
   }
   provisioner "remote-exec" {
      inline = [
-        "rm -f ~/secrets.json ~/app.json"
+        "rm -f ~/*.json"
     ]
   }
 }
