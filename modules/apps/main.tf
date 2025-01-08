1dbf5a77096b1adc4494a7b88db112f3502599f3
shuja <shuja.devops@gmail.com>
2025-01-08 14:47:34 +0530

    listeners changed

--- modules/apps/main.tf
+++ modules/apps/main.tf
@@ -163,41 +163,40 @@ resource "aws_lb_target_group_attachment" "tg-attachment" {
   port             = var.app_port
 }
 
-# resource "aws_lb_listener" "frontend-http" {
-#   count            = var.lb_needed && var.lb_type == "public"? 1 : 0
-#   load_balancer_arn = aws_lb.main[0].arn
-#   port              = var.app_port
-#   protocol          = "HTTP"
-#
-#   default_action {
-#     type = "redirect"
-#
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
+resource "aws_lb_listener" "frontend-http" {
+  count            = var.lb_needed && var.lb_type == "public"? 1 : 0
+  load_balancer_arn = aws_lb.main[0].arn
+  port              = var.app_port
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
 #
-# resource "aws_lb_listener" "frontend-https" {
-#   count            = var.lb_needed ? 1 : 0
-#   load_balancer_arn = aws_lb.main[0].arn
-#   port              = var.app_port
-#   protocol          = "HTTP"
-#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-#   certificate_arn   = var.certificate_arn
-#
-#
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.lb-tg[0].arn
-#   }
-# }
+resource "aws_lb_listener" "frontend-https" {
+  count            = var.lb_needed && var.lb_type == "public"? 1 : 0
+  load_balancer_arn = aws_lb.main[0].arn
+  port              = var.app_port
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main[0].arn
+  }
+}
 
 resource "aws_lb_listener" "backend" {
-  count            = var.lb_needed ? 1 : 0
+  count             = var.lb_needed && var.lb_type != "public"? 1 : 0
   load_balancer_arn = aws_lb.main[0].arn
   port              = var.app_port
   protocol          = "HTTP"
