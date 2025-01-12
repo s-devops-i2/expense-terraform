ed041fdcf37d77c918d29bdea5be435791e15397
shuja <shuja.devops@gmail.com>
2025-01-12 14:53:36 +0530

    user and password manually passed

--- modules/apps/main.tf
+++ modules/apps/main.tf
@@ -64,8 +64,10 @@ resource "null_resource" "ansible" {
 
   connection {
     type     = "ssh"
-    user     =  jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
-    password =  jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+#    user     =  jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
+#    password =  jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
+     user     = "ec2-user"
+     password = "DevOps321"
     host     = aws_instance.instance.private_ip
   }
     provisioner "remote-exec" {
