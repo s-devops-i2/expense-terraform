292a67a0bdf4f3830fa017d90b76a016cc7e9a62
shuja <shuja.devops@gmail.com>
2025-01-16 14:32:11 +0530

    enabled mysql to test

--- main.tf
+++ main.tf
@@ -1,46 +1,46 @@
-# module "frontend" {
-#   depends_on    = [module.backend]
-#   source        = "./modules/apps"
-#   instance_type = var.instance_type
-#   component     ="frontend"
-#   env           = var.env
-#   zone_id       = var.zone_id
-#   vault_token   = var.vault_token
-#   subnets       = module.vpc.frontend_subnet
-#   vpc_id        = module.vpc.vpc_id
-#   lb_type       = "public"
-#   lb_needed     = true
-#   lb_subnet     = module.vpc.public_subnet
-#   app_port      = 80
-#   certificate_arn = var.certificate_arn
-#   bastion_nodes   = var.bastion_nodes
-#   prometheus_nodes = var.prometheus_nodes
-#   server_app_port_sg_cidr = var.public_subnets
-#   lb_app_port_sg_cidr     = ["0.0.0.0/0"]
-#   lb_ports                = {http: 80, https: 443}
-# }
-#
-# module "backend" {
-#   depends_on    = [module.mysql]
-#
-#   source        = "./modules/apps"
-#   instance_type = var.instance_type
-#   component     ="backend"
-#   env           = var.env
-#   zone_id       = var.zone_id
-#   vault_token   = var.vault_token
-#   subnets       = module.vpc.backend_subnet
-#   vpc_id        = module.vpc.vpc_id
-#   lb_type       = "private"
-#   lb_needed     = true
-#   lb_subnet     = module.vpc.backend_subnet
-#   app_port       = 8080
-#   bastion_nodes  = var.bastion_nodes
-#   prometheus_nodes = var.prometheus_nodes
-#   server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
-#   lb_app_port_sg_cidr     = var.frontend_subnets
-#   lb_ports                = {http: 8080}
-# }
+module "frontend" {
+  depends_on    = [module.backend]
+  source        = "./modules/apps"
+  instance_type = var.instance_type
+  component     ="frontend"
+  env           = var.env
+  zone_id       = var.zone_id
+  vault_token   = var.vault_token
+  subnets       = module.vpc.frontend_subnet
+  vpc_id        = module.vpc.vpc_id
+  lb_type       = "public"
+  lb_needed     = true
+  lb_subnet     = module.vpc.public_subnet
+  app_port      = 80
+  certificate_arn = var.certificate_arn
+  bastion_nodes   = var.bastion_nodes
+  prometheus_nodes = var.prometheus_nodes
+  server_app_port_sg_cidr = var.public_subnets
+  lb_app_port_sg_cidr     = ["0.0.0.0/0"]
+  lb_ports                = {http: 80, https: 443}
+}
+
+module "backend" {
+  depends_on    = [module.mysql]
+
+  source        = "./modules/apps"
+  instance_type = var.instance_type
+  component     ="backend"
+  env           = var.env
+  zone_id       = var.zone_id
+  vault_token   = var.vault_token
+  subnets       = module.vpc.backend_subnet
+  vpc_id        = module.vpc.vpc_id
+  lb_type       = "private"
+  lb_needed     = true
+  lb_subnet     = module.vpc.backend_subnet
+  app_port       = 8080
+  bastion_nodes  = var.bastion_nodes
+  prometheus_nodes = var.prometheus_nodes
+  server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
+  lb_app_port_sg_cidr     = var.frontend_subnets
+  lb_ports                = {http: 8080}
+}
 # module "rds" {
 #   source = "./modules/rds"
 #
@@ -58,20 +58,20 @@
 #   vpc_id                    = module.vpc.vpc_id
 # }
 #
-# module "mysql" {
-#   source        = "./modules/apps"
-#   instance_type = var.instance_type
-#   component     ="mysql"
-#   env           = var.env
-#   zone_id       = var.zone_id
-#   vault_token   = var.vault_token
-#   subnets       = module.vpc.db_subnet
-#   vpc_id        = module.vpc.vpc_id
-#   app_port      = 3306
-#   bastion_nodes  = var.bastion_nodes
-#   prometheus_nodes = var.prometheus_nodes
-#   server_app_port_sg_cidr     = var.backend_subnets
-# }
+module "mysql" {
+  source        = "./modules/apps"
+  instance_type = var.instance_type
+  component     ="mysql"
+  env           = var.env
+  zone_id       = var.zone_id
+  vault_token   = var.vault_token
+  subnets       = module.vpc.db_subnet
+  vpc_id        = module.vpc.vpc_id
+  app_port      = 3306
+  bastion_nodes  = var.bastion_nodes
+  prometheus_nodes = var.prometheus_nodes
+  server_app_port_sg_cidr     = var.backend_subnets
+}
 
 module "vpc" {
   source                = "./modules/vpc"
