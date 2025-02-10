d83b99203df8a4970e16549c3e1a7a2a321a0f41
shuja-git <shuja.devops@gmail.com>
2025-02-09 23:08:51 -0800

    DNS changed

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address = "https://vault-internal.shujathdevops.online:8200"
+  address = "https://vault-internal.shujadevops.shop:8200"
   token = var.vault_token
   skip_tls_verify = true
 }
